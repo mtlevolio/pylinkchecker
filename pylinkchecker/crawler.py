b111e297e83eeb4290b151529d862a1e49dc5033
Barthelemy Dagenais <bdagenais@auto123.com>
2013-06-29 15:46:22 -0400

    command line arguments for configuration

--- pylinkchecker/crawler.py
+++ pylinkchecker/crawler.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Contains the main crawling logic.
+"""
+from __future__ import unicode_literals, absolute_import
+
+from pylinkchecker.models import get_parser
+
 
 def execute_from_command_line():
-    pass
+    parser = get_parser()
+    (options, args) = parser.parse_args()
