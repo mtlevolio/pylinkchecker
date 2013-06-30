dfbe14194f6ae17e1c93c676b9f859d92b3395e4
Barthelemy Dagenais <bdagenais@auto123.com>
2013-06-30 07:08:40 -0400

    added simple test to kickoff testing

---
+++ pylinkchecker/tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Unit and integration tests for pylinkchecker
+"""
+from __future__ import unicode_literals, absolute_import
+
+import unittest
+
+
+class ConfigTest(unittest.TestCase):
+
+    def test_simple(self):
+        self.assertTrue(True)
